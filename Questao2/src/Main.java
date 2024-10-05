9e3151c43f736682b94de2df242ebe5f80792feb
João Victor <joaovictorlira18@hotmail.com>
2024-10-04 21:57:49 -0300

    last

--- Questao2/src/Main.java
+++ Questao2/src/Main.java
@@ -13,7 +13,9 @@ class Produto {
 
     public Produto() {
 
-    };
+    }
+
+    ;
 
     // Construtor
     public Produto(String codigo, String nome, String tamanhoOuPeso, String cor, double valor, int quantidadeEstoque) {
@@ -36,7 +38,9 @@ class Produto {
     }
 
     public void atualizarEstoque(int quantidadeVendida) {
-        this.quantidadeEstoque -= quantidadeVendida;
+        if (quantidadeVendida <= quantidadeEstoque) {
+            this.quantidadeEstoque -= quantidadeVendida;
+        }
     }
 
 }
@@ -44,95 +48,113 @@ class Produto {
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         ArrayList<Produto> listaProdutos = new ArrayList<>();
+        Produto novoProduto = null;
 
-        System.out.println("Cadastro de produto");
+        
+        System.out.println("Você deseja cadastrar um produto , ou realizar a compra?");
+        System.out.println("1 - cadastrar 2 - comprar");
+        int opcaoCadastroCompra = sc.nextInt();
 
+        switch (opcaoCadastroCompra) {
 
-        System.out.println("Digite o codigo");
-        String codigo = sc.nextLine();
+            case 1:
 
-
-        System.out.println("Digite o nome");
-        String nome = sc.nextLine();
-
-        System.out.println("Digite o tamanho ou peso");
-        String tamanhoOuPeso = sc.nextLine();
-
-        System.out.println("Digite a cor");
-        String cor = sc.nextLine();
-
-        System.out.println("Digite o valor");
-        double valor = sc.nextDouble();
-
-        System.out.println("Digite a quantidade em estoque");
-        int quantidadeEstoque = sc.nextInt();
-        sc.nextLine();
-
-        Produto novoProduto = new Produto(codigo, nome, tamanhoOuPeso, cor, valor, quantidadeEstoque);
-        listaProdutos.add(novoProduto);
-
-        System.out.println("produto cadastrado com sucesso!");
-
-        novoProduto.exibirProduto();
+                System.out.println("Cadastro de produto");
 
 
-
-/*
-        System.out.println("Quantas unidades deseja comprar?");
-        int quantidadeVendida = sc.nextInt();
-
-        if (quantidadeVendida <= produto.quantidadeEstoque) {
-            double valorTotal = produto.valor * quantidadeVendida;
-            System.out.println("Valor total sem desconto: R$ " + valorTotal);
-
-            System.out.println("\nFormas de pagamentos disponíveis");
-            System.out.println("1 - Pix\n2 - Espécie\n3 - Transferência\n4 - Débito\n5 - Crédito");
-            int formaPagamento = sc.nextInt();
-
-            double desconto = 0;
+                System.out.println("Digite o codigo");
+                String codigo = sc.nextLine();
 
 
-            switch (formaPagamento) {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                    desconto = 0.05 * valorTotal;
-                    valorTotal -= desconto;
-                    System.out.println("Valor total com desconto de 5% aplicado" + valorTotal);
+                System.out.println("Digite o nome");
+                String nome = sc.nextLine();
+
+                System.out.println("Digite o tamanho ou peso");
+                String tamanhoOuPeso = sc.nextLine();
+
+                System.out.println("Digite a cor");
+                String cor = sc.nextLine();
+
+                System.out.println("Digite o valor");
+                double valor = sc.nextDouble();
+
+                System.out.println("Digite a quantidade em estoque");
+                int quantidadeEstoque = sc.nextInt();
+                sc.nextLine();
+
+                novoProduto = new Produto(codigo, nome, tamanhoOuPeso, cor, valor, quantidadeEstoque);
+                listaProdutos.add(novoProduto);
+                novoProduto.exibirProduto();
+
+                System.out.println("produto cadastrado com sucesso!");
+                break;
+            case 2:
+                if(novoProduto == null) {
+                    System.out.println("Produto não cadastrado");
                     break;
-                case 5:
-                    System.out.println("Você deseja parcelar em 3x sem juros? (1- Sim, 2- Não)");
-                    int opcaoParcelamento = sc.nextInt();
+                }
 
-                    if (opcaoParcelamento == 1) {
-                        System.out.println("Valor parcelado em 3x de R$ " + valorTotal / 3);
-                    } else if (opcaoParcelamento == 2) {
-                        System.out.println("Digite o valor pago");
-                        double valorPago = sc.nextInt();
+                System.out.println("Quantas unidades deseja comprar?");
+                int quantidadeVendida = sc.nextInt();
 
-                        if (valorPago > valorTotal) {
-                            double troco = valorPago - valorTotal;
-                            System.out.println("Troco R$ " + troco);
-                        } else if (valorPago < valorTotal) {
-                            System.out.println("Valor insuficiente");
-                        }
+                if (quantidadeVendida <= novoProduto.quantidadeEstoque) {
+                    double valorTotal = novoProduto.valor * quantidadeVendida;
+                    System.out.println("Valor total sem desconto: R$ " + valorTotal);
+
+                    System.out.println("\nFormas de pagamentos disponíveis");
+                    System.out.println("1 - Pix\n2 - Espécie\n3 - Transferência\n4 - Débito\n5 - Crédito");
+                    int formaPagamento = sc.nextInt();
+
+                    double desconto = 0;
+
+
+                    switch (formaPagamento) {
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                            desconto = 0.05 * valorTotal;
+                            valorTotal -= desconto;
+                            System.out.println("Valor total com desconto de 5% aplicado" + valorTotal);
+                            break;
+                        case 5:
+                            System.out.println("Você deseja parcelar em 3x sem juros? (1- Sim, 2- Não)");
+                            int opcaoParcelamento = sc.nextInt();
+
+                            if (opcaoParcelamento == 1) {
+                                System.out.println("Valor parcelado em 3x de R$ " + valorTotal / 3);
+                            } else if (opcaoParcelamento == 2) {
+                                System.out.println("Digite o valor pago");
+                                double valorPago = sc.nextInt();
+
+                                if (valorPago > valorTotal) {
+                                    double troco = valorPago - valorTotal;
+                                    System.out.println("Troco R$ " + troco);
+                                } else if (valorPago < valorTotal) {
+                                    System.out.println("Valor insuficiente");
+                                }
+                            }
+                            break;
+                        default:
+                            System.out.println("Forma de pagamento inválida");
+                            return;
                     }
-                    break;
-                default:
-                    System.out.println("Forma de pagamento inválida");
-                    return;
-            }
-        } else {
-            System.out.println("Estoque insuficiente!");
+                } else {
+                    System.out.println("Estoque insuficiente!");
+                }
+
+                novoProduto.atualizarEstoque(quantidadeVendida);
+                System.out.println("Venda realizada com sucesso!");
+                System.out.println("Quantidades em estoque restantes " + novoProduto.quantidadeEstoque);
+                break;
+
+            default:
+                System.out.println("Opcão inválida!");
         }
 
-        produto.atualizarEstoque(quantidadeVendida);
-        System.out.println("Venda realizada com sucesso!");
-        System.out.println("Quantidades em estoque restantes "+ produto.quantidadeEstoque);
-*/
+        sc.close();
+
 
     }
 }
